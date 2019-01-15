f61d9d730d4cdb537dc8d48bad58ac3a5622d8f5
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-15 10:55:20 +0800

    Update SpringDemoApplication.java

--- src/main/java/com/lori/springdemo/SpringDemoApplication.java
+++ src/main/java/com/lori/springdemo/SpringDemoApplication.java
@@ -3,11 +3,23 @@ package com.lori.springdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringDemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringDemoApplication.class, args);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("输入quit,会优雅退出");
+
+		String line = scanner.nextLine();
+
+		if ("quit".equals(line)){
+			System.exit(1);
+		}
 	}
 
 }
