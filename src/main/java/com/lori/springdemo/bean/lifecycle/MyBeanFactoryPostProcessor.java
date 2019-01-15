603e91daf40d33ab4257d582ab00ae488a5f9729
lori <40824190+lalaoneo@users.noreply.github.com>
2019-01-15 10:53:44 +0800

    Update MyBeanFactoryPostProcessor.java

--- src/main/java/com/lori/springdemo/bean/lifecycle/MyBeanFactoryPostProcessor.java
+++ src/main/java/com/lori/springdemo/bean/lifecycle/MyBeanFactoryPostProcessor.java
@@ -6,17 +6,31 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Component
 public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
 
+    // 增加打印日志标志,只打印一次
+    private AtomicBoolean myBeanFactoryPostProcessorLogFlag = new AtomicBoolean(true);
+
+    private AtomicBoolean postProcessBeanFactoryLogFlag = new AtomicBoolean(true);
+
     public MyBeanFactoryPostProcessor(){
         super();
-        System.out.println("[lori]这是BeanFactoryPostProcessor实现类构造器！！");
+        if (myBeanFactoryPostProcessorLogFlag.getAndSet(false)){
+            System.out.println("[lori：step_1] 这是BeanFactoryPostProcessor实现类构造器！！");
+        }
     }
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
-        System.out.println("[lori]BeanFactoryPostProcessor调用postProcessBeanFactory方法");
+        if (postProcessBeanFactoryLogFlag.getAndSet(false)){
+            System.out.println("[lori：step_2] BeanFactoryPostProcessor调用postProcessBeanFactory方法");
+        }
         BeanDefinition bd = configurableListableBeanFactory.getBeanDefinition("person");
+        bd.getPropertyValues().addPropertyValue("name","神探");
+        bd.getPropertyValues().addPropertyValue("address","地球");
         bd.getPropertyValues().addPropertyValue("phone","110");
+
     }
 }
